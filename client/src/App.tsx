9351366c7bbc18356f1310d3b0311a1adf30e4c6
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2024-01-25 16:18:02 +0530

    client and api initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignIn />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        error: 'var(--error)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        success: 'var(--success)',
+        primary2: 'var(--primary2)',
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      text: {
+        primary: 'rgb(0, 167, 111)',
+      },
+      screens: {
+        xxs:"360px",
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+    },
+    },
+  },
+}
+
