caf9dc04dbfda34a15c039cdc5eeb453a78dcaa9
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2024-01-26 18:40:55 +0530

    Connecting to mongo database with mongoose

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 import express from "express"
+import mongoose from "mongoose"
+import "dotenv/config"
 
+mongoose
+  .connect(process.env.MONGOOSE_URI)
+  .then(() => {
+    console.log("Connected to mongodb!")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+  
 const app = express()
 
 const PORT = 7000
 
 app.listen(PORT, () => {
-    console.log(`Server running on port:${PORT}`)
+  console.log(`Server running on port:${PORT}`)
 })
