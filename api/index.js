ede50198c1be759b28cdede54cf66534936d86a4
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2024-01-30 12:53:31 +0530

    added auth route and created a new user via insomnia

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import "dotenv/config"
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
 mongoose
   .connect(process.env.MONGOOSE_URI)
@@ -14,8 +15,11 @@ mongoose
   
 const app = express()
 
+app.use(express.json())
+
 app.listen(process.env.LOCAL_PORT, () => {
   console.log(`Server running on port:${process.env.LOCAL_PORT}`)
 })
 
 app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
