eb1f58a5509e35628dc0c57abe93a061a95ceaa0
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2024-02-02 21:22:59 +0530

    backend: Completed Google OAuth functionality

--- client/src/components/OAuth.tsx
+++ client/src/components/OAuth.tsx
@@ -2,9 +2,11 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth"
 import {app} from "../firebase"
 import { useDispatch } from "react-redux"
 import { signInSuccess } from "../slices/user/userSlice"
+import { useNavigate } from "react-router-dom"
 
 const OAuth = () => {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     const handleGoogleClick = async () => {
         try {
             const provider = new GoogleAuthProvider()
@@ -22,16 +24,15 @@ const OAuth = () => {
             const data = await res.json()
             console.log(data)
             dispatch(signInSuccess(data))
+            navigate("/")
         } catch (error) {
             console.log('Could not signin with google', error)
         }
     }
   return (
-    <div>
       <button type="button" onClick={handleGoogleClick} className="bg-red-500 text-white p-3 rounded-lg uppercase hover:opacity-95">
         Continue with Google
       </button>
-    </div>
   )
 }
 
