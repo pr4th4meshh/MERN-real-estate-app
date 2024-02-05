7ee09536fda5589ff2b68b9ad09f21b04992d1d6
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2024-02-05 21:04:21 +0530

    Created delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -37,6 +37,18 @@ export const updateUser = async (req, res, next) => {
   }
 }
 
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account!"))
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.clearCookie("access_token")
+    res.status(200).json({message: "User has been deleted..."})
+  } catch (error) {
+    next(error)
+  }
+}
+
 export const getUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id)
--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -7,7 +7,7 @@ import {
 import { useEffect, useRef, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { app } from "../firebase"
-import { updateUserFailure, updateUserStart, updateUserSuccess } from "../slices/user/userSlice"
+import { deleteUserFailure, deleteUserStart, deleteUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from "../slices/user/userSlice"
 
 export default function Profile() {
   //App states
@@ -83,6 +83,23 @@ export default function Profile() {
     setFormData({...formData, [e.target.id]: e.target.value })
   }
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart())
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      })
+      const data = await res.json()
+      if(data.success === false) {
+        dispatch(deleteUserFailure(data.message))
+        return;
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch(deleteUserFailure(error))
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -143,7 +160,7 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-500 text-sm">{error? error : ""}</p>
