bc0af81df93d4fc30ca5739379baf182a2f2e2cb
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2024-02-09 23:07:11 +0530

    Created contact landlord functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -43,13 +43,27 @@ export const deleteUser = async (req, res, next) => {
   try {
     await User.findByIdAndDelete(req.params.id)
     res.clearCookie("access_token")
-    res.status(200).json({message: "User has been deleted..."})
+    res.status(200).json({ message: "User has been deleted..." })
   } catch (error) {
     next(error)
   }
 }
 
 export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id)
+
+    if (!user) return next(errorHandler(404, "User not found!"))
+
+    const { password: pass, ...rest } = user._doc
+
+    res.status(200).json(rest)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getUserDetails = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id)
 
