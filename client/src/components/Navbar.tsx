c5435feb4a0c11c6e9a58859adbdc676651ab542
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2024-02-10 20:37:19 +0530

    Completed header search functionality

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,27 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { FaSearch } from "react-icons/fa"
 import { useSelector } from "react-redux"
+import { useEffect, useState } from "react"
 
 const Navbar = () => {
   const { currentUser } = useSelector((state: unknown) => state.user)
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <div>
       <header className="bg-slate-200 shadow-md">
@@ -15,15 +33,15 @@ const Navbar = () => {
             </h1>
           </Link>
           <form
-            //   onSubmit={handleSubmit}
+              onSubmit={handleSubmit}
             className="bg-slate-100 p-3 rounded-lg flex items-center"
           >
             <input
               type="text"
               placeholder="Search..."
               className="bg-transparent focus:outline-none w-24 sm:w-64"
-              // value={searchTerm}
-              // onChange={(e) => setSearchTerm(e.target.value)}
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
             <button>
               <FaSearch className="text-slate-600" />
